f8ecfeaef5a99ba9f64bec2dbecc11ea8bd07e3c
kadir <idia_2008@hotmail.com>
2023-02-12 19:50:01 +0300

    start main js

---
+++ whats-app/main.js
@@ -0,0 +1,9 @@
+let tabsLis = document.querySelectorAll('.tabs li');
+    boxes = document.querySelectorAll('.content .box');
+
+tabsLis.forEach((tab) => {
+    // Add Click For Each li
+    tab.addEventListener('click', function(e) {
+        
+    })
+})
